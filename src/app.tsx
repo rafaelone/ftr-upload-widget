0536c53cdf848e38164c51a21bca278a364319f0
Rafael <rafael.sergio@nextpage.com.br>
2025-01-23 16:35:36 -0300

    style: dropzone component

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,9 @@
-
-
+import { UploadWidget } from './components/upload-widget'
 
 export function App() {
-  
-
   return (
-   <h1>Hello worlds</h1>
+    <main className="h-dvh flex flex-col items-center justify-center p-10">
+      <UploadWidget />
+    </main>
   )
 }
-
-
---
+++ src/components/upload-widget.tsx
@@ -0,0 +1,27 @@
+import * as Collapsible from '@radix-ui/react-collapsible'
+import { useState } from 'react'
+
+import { UploadWidgetDropzone } from './upload-widget-dropzone'
+import { UploadWidgetHeader } from './upload-widget-header'
+import { UploadWidgetMinimizedButton } from './upload-widget-minimized-button'
+import { UploadWidgetUploadList } from './upload-widget-upload-list'
+
+export function UploadWidget() {
+  const [isWidgetOpen, setIsWidgetOpen] = useState<boolean>(false)
+
+  return (
+    <Collapsible.Root onOpenChange={setIsWidgetOpen}>
+      <div className="bg-zinc-900 w-[360px] rounded-xl shadow-shape  overflow-hidden">
+        {!isWidgetOpen && <UploadWidgetMinimizedButton />}
+        <Collapsible.Content>
+          <UploadWidgetHeader />
+          <div className="flex flex-col gap-4 py-3">
+            <UploadWidgetDropzone />
+            <div className="h-px bg-zinc-800 border-t border-black/50 box-content" />
+            <UploadWidgetUploadList />
+          </div>
+        </Collapsible.Content>
+      </div>
+    </Collapsible.Root>
+  )
+}
