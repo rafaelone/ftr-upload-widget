b5b30ef872455eaa8275862f8b0c93dbbaff7ef4
Rafael <rafael.sergio@nextpage.com.br>
2025-01-23 17:24:42 -0300

    style: animation with motion

--- src/components/upload-widget-dropzone.tsx
+++ src/components/upload-widget-dropzone.tsx
@@ -1,6 +1,12 @@
+import { motion } from 'motion/react'
 import { useDropzone } from 'react-dropzone'
 
+import CircularProgressBar from './ui/circular-progress-bar'
+
 export function UploadWidgetDropzone() {
+  const isThereAnyPendingUpload = true
+  const uploadGlobalPercentage = 66
+
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
     multiple: true,
     accept: {
@@ -11,19 +17,37 @@ export function UploadWidgetDropzone() {
   })
 
   return (
-    <div className="px-3 flex flex-col gap-3">
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.2 }}
+      className="px-3 flex flex-col gap-3"
+    >
       <div
         data-active={isDragActive}
-        className="cursor-pointer text-zinc-400 bg-black/20 p-5 rounded-lg border border-zinc-700 border-dashed h-32 flex flex-col items-center justify-center gap-1 hover:border-zinc-600 transition-colors data-[active=true]:bg-indigo-500/10 data-[active=true]:border-indigo-500 data-[active=true]:text-indigo-400"
+        className="cursor-pointer text-zinc-400 bg-black/20 p-5 rounded-lg border border-zinc-700 border-dashed h-32 flex flex-col items-center justify-center gap-1 hover:border-zinc-600 transition-colors data-[active=true]:bg-indigo-500/10 data-[active=true]:border-indigo-500"
         {...getRootProps()}
       >
         <input type="file" {...getInputProps()} />
-        <span className="text-xs">Drop your files here or</span>
-        <span className="text-xs underline">click to open picker </span>
+        {isThereAnyPendingUpload ? (
+          <div className="flex flex-col gap-2.5 items-center">
+            <CircularProgressBar
+              progress={uploadGlobalPercentage}
+              size={56}
+              strokeWidth={4}
+            />
+            <span>Uploading 2 files...</span>
+          </div>
+        ) : (
+          <>
+            <span className="text-xs">Drop your files here or</span>
+            <span className="text-xs underline">click to open picker </span>
+          </>
+        )}
       </div>
-      <span className="text-xs text-zinc-400">
+      <span className="text-xxs text-zinc-400">
         Only PNG and JPG files are supported.
       </span>
-    </div>
+    </motion.div>
   )
 }
--- src/components/upload-widget-upload-list.tsx
+++ src/components/upload-widget-upload-list.tsx
@@ -1,3 +1,23 @@
+import { UploadWidgetUploadItem } from './upload-widget-upload-item'
+
 export function UploadWidgetUploadList() {
-  return <div className="px-3 h-48"></div>
+  const isUploadListEmpty = true
+
+  return (
+    <div className="px-3 flex flex-col gap-3">
+      <span className="text-xs font-medium">
+        Uploaded files <span className="text-zinc-400">(2)</span>
+      </span>
+      {!isUploadListEmpty ? (
+        <span className="text-xs text-zinc-400">No uploads added</span>
+      ) : (
+        <div className="flex flex-col gap-2">
+          <UploadWidgetUploadItem />
+          <UploadWidgetUploadItem />
+          <UploadWidgetUploadItem />
+          <UploadWidgetUploadItem />
+        </div>
+      )}
+    </div>
+  )
 }
